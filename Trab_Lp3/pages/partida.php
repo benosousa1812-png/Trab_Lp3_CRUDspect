d8bb2d0ccaa61e1e10b25c28679d680c4c6eeb39
logan1654x <logan1654x@gmail.com>
2026-06-26 14:47:35 -0300

    estilização da partida v1
    comecei a estilizar a os containers dos personagens e do boss

--- Trab_Lp3/pages/partida.php
+++ Trab_Lp3/pages/partida.php
@@ -5,105 +5,204 @@ require_once __DIR__ . '/../includes/auth.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Partida RPG</title>
+
     <link rel="stylesheet" href="../assets/style.css">
+
 </head>
+
 <body class="partida_pag">
 
-    <!-- PERSONAGENS -->
-    <div id="info"></div>
+<div id="info"></div>
 
-    <!-- Boss -->
-    <div id="boss_container">
-        <!-- aqui fazer a logica de aparecer cada foto do boss dependendo de qual tema foi escolhido na partida (obs as imagens estao na pasta bosses com suas habilidades) !-->
-       
-    </div>
+<div id="boss_container">
+<!-- logica do boss na func iniciarpartida na linha 100-->
+</div>
 
-    <!-- HABILIDADES FIXAS -->
-    <div id="habilidades-container">
-        <ul id="habilidades-list"></ul>
-    </div>
+<div id="habilidades-container">
 
-    <script>
-        async function iniciarPartida() {
-            //pega os dados q fez no partida_create (pers, dif e local) pra usar
-            const partida = JSON.parse(localStorage.getItem("partida"));
+    <ul id="habilidades-list"></ul>
 
-            if (!partida) {
-                alert("Partida não encontrada!");
-                return;
-            }
+</div>
 
-            // Aplica classe de fundo
-            document.body.classList.add(partida.local);
-            // mostra a fotinha do boss na direita (depende do tema escolhido no partuda_create)
-            const bossContainer = document.getElementById("boss_container");
+<script>
 
-            if (partida.local === "deserto") {
-                bossContainer.innerHTML =
-                    '<img src="../bosses/deserto/imagem do deserto.png" alt="Boss Deserto">';
-            }
-            else if (partida.local === "floresta") {
-                bossContainer.innerHTML =
-                    '<img src="../bosses/floresta/boss_floresta.png" alt="Boss Floresta">';
-            }
-            else if (partida.local === "montanha") {
-                bossContainer.innerHTML =
-                    '<img src="../bosses/montanha/boss_montanha.png" alt="Boss Montanha">';
-            }
+// GUARDA A VIDA DA PARTIDA
+let batalha = {
+    jogadores:[
 
-            
+        {
+            id:1,
+            nome:"",
+            vida:100,
+            vidaMax:100
+        },
 
-            try {
-                // Busca personagens via API
-                const response = await fetch("partida_perso_pegar.php", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify({ personagens: partida.personagens })
-                });
+        {
+            id:2,
+            nome:"",
+            vida:100,
+            vidaMax:100
+        },
 
-                if (!response.ok) throw new Error("Erro ao buscar personagens da partida");
-
-                const personagens = await response.json();
-
-                const containerPersonagens = document.getElementById("info");
-                const containerHabilidades = document.getElementById("habilidades-list");
-
-                containerPersonagens.innerHTML = "";
-                containerHabilidades.innerHTML = "";
-
-                personagens.forEach(p => {
-                    const div = document.createElement("div");
-                    div.className = "personagem";
-
-                    div.innerHTML = `
-                        <img src="../${p.imagem}" alt="${p.nome}">
-                        <strong>${p.nome}</strong>
-                    `;
-
-                    // Ao clicar no personagem, mostra suas habilidades com dano e cura
-                    div.addEventListener("click", () => {
-                    containerHabilidades.innerHTML = "";
-                    p.habilidades.forEach(h => {
-                    const li = document.createElement("li");
-                    li.textContent = `${h.nome} - Tipo: ${h.tipo} - Dano: ${h.dano} - Cura: ${h.cura ?? 0}`;
-                    containerHabilidades.appendChild(li);
-                        });
-                    });
-
-                    containerPersonagens.appendChild(div);
-                });
-
-            } catch (err) {
-                console.error(err);
-                document.getElementById("info").innerHTML = "Erro ao carregar personagens da partida.";
-            }
+        {
+            id:3,
+            nome:"",
+            vida:100,
+            vidaMax:100
         }
+    ],
 
-        iniciarPartida();
-    </script>
+    boss:{
+        id:"boss",
+        nome:"Boss",
+        vida:200,
+        vidaMax:200
+    }
+};
+
+// CRIA A BARRA DE VIDA
+function criarBarraVida(atual,max,tipo){
+    let porcentagem = (atual / max) * 100;
+
+    return `
+
+        <div class="vida_texto">
+
+            Vida: ${atual}/${max}
+
+        </div>
+
+        <div class="barra">
+            <div 
+            class="vida ${tipo}" 
+            style="width:${porcentagem}%">
+            </div>
+        </div>
+    `;
+}
+
+
+async function iniciarPartida(){
+
+    const partida = JSON.parse(localStorage.getItem("partida"));
+
+    if(!partida){
+        alert("Partida não encontrada!");
+        return;
+    }
+    document.body.classList.add(partida.local);
+
+    const bossContainer = document.getElementById("boss_container");
+    if(partida.local === "deserto"){
+        bossContainer.innerHTML =
+        '<img src="../bosses/deserto/imagem do deserto.png">';
+    }
+    else if(partida.local === "floresta"){
+        bossContainer.innerHTML =
+        '<img src="../bosses/floresta/boss_floresta.png">';
+    }
+    else if(partida.local === "montanha"){
+        bossContainer.innerHTML =
+        '<img src="../bosses/montanha/boss_montanha.png">';
+    }
+    // barra do boss embaixo da imagem
+    bossContainer.innerHTML += `
+        <div class="boss_vida">
+        ${criarBarraVida(
+            batalha.boss.vida,
+            batalha.boss.vidaMax,
+            "boss"
+        )}
+        </div>
+    `;
+    try{
+        const response = await fetch("partida_perso_pegar.php",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                personagens:partida.personagens
+            })
+        });
+        if(!response.ok)
+
+            throw new Error("Erro ao buscar personagens");
+
+        const personagens = await response.json();
+
+        const containerPersonagens =
+        document.getElementById("info");
+
+        const containerHabilidades =
+        document.getElementById("habilidades-list");
+
+        containerPersonagens.innerHTML="";
+        containerHabilidades.innerHTML="";
+        personagens.forEach((p,index)=>{
+            const jogador = batalha.jogadores[index];
+            jogador.nome = p.nome;
+            const div = document.createElement("div");
+            div.className="personagem";
+            div.innerHTML = `
+                <img src="../${p.imagem}" alt="${p.nome}">
+                <strong>
+
+                Jogador ${jogador.id} - ${p.nome}
+
+                </strong>
+                ${criarBarraVida(
+                    jogador.vida,
+                    jogador.vidaMax,
+                    "player"
+                )}
+            `;
+            div.addEventListener("click",()=>{
+                containerHabilidades.innerHTML="";
+                p.habilidades.forEach(h=>{
+                    const li = document.createElement("li");
+                    li.textContent = 
+                    `${h.nome} - Tipo: ${h.tipo} - Dano: ${h.dano} - Cura: ${h.cura ?? 0}`;
+                    containerHabilidades.appendChild(li);
+                    li.addEventListener("click",()=>{
+                        ver_ação(
+                            h.nome,
+                            h.tipo,
+                            h.dano,
+                            h.cura
+                        );
+                    });
+                });
+            });
+            containerPersonagens.appendChild(div);
+        });
+    }
+
+    catch(err){
+        console.error(err);
+        document.getElementById("info").innerHTML =
+        "Erro ao carregar personagens da partida.";
+
+    }
+
+}
+
+iniciarPartida();
+
+function ver_ação(nome,tipo,dano,cura){
+    console.log("clicou na lista de habilidades");
+    console.log("Nome:",nome);
+    console.log("Tipo:",tipo);
+    console.log("Dano:",dano);
+    console.log("Cura:",cura);
+}
+</script>
 </body>
 </html>
